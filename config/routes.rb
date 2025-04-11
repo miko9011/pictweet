d4037a3c3c337bc84ffca704fbc2b9f4d52ff629
seku <gamerrrr0911@gmail.com>
2025-04-11 10:40:10 +0900

    投稿画面の編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'tweets#index'
-  resources :tweets do
-    resources :comments, only: :create
-    collection do
-      get 'search'
-    end
-  end
-  resources :users, only: :show
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
 end
